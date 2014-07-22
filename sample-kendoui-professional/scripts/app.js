03f96a3d8316b3106d111828f05eb854dd636bef
Yanaki Yanakiev <yanaki.yanakiev@telerik.com>
2014-07-22 08:22:47 -0500

    Style message for add kendo professional

--- sample-kendoui-professional/scripts/app.js
+++ sample-kendoui-professional/scripts/app.js
@@ -3,16 +3,15 @@
         mobileSkin = "",
         defaultChartTheme = "silver",
         app = global.app = global.app || {};
-
-    document.addEventListener("DOMContentLoaded", function() {
-    }, false);
     
     document.addEventListener("deviceready", function () {
         navigator.splashscreen.hide();
         if (typeof kendo !== "undefined" ) {
             application = new kendo.mobile.Application(document.body, { transition: "", layout: "mobile-tabstrip"});
         } else {
-            document.body.innerHTML = "To run this sample, install Kendo UI Professional manually. For more information, see the sample readme.";
+            document.body.innerHTML = "To run this sample, install Kendo UI Professional manually from manage packages.<br> "+
+           						   "For more information, see the sample readme.";
+            document.body.className = "note";
         }
     }, false);
     
